6a38e56b0e8b844322cd712d592619633a260680
pi <pi>
2013-02-05 02:32:39 +0000

    modifying capture - need fields aliging correction

--- bin/capture.py
+++ bin/capture.py
@@ -1,10 +1,24 @@
 #!/usr/bin/python
 import sys
 
+count=0
 while 1:
     try:
         line = sys.stdin.readline()
-	fields = line.split()
+	fields = line.split('\t')
+	if (len(fields[4])>0):
+		sourcemac=fields[4]
+	else if (len(fields[2])>0):
+		sourcemac=fields[2]
+	else
+		continue;
+		#no sa or ta
+	
+
+	count+=1
+	if(count>1000):
+		count=0
+		display_update()
 	
 
     except KeyboardInterrupt:
@@ -14,4 +28,8 @@ while 1:
         break
 
     print >> sys.stderr, fields
+
+
+def display_update():
+
 
