bcfeeeb527dfd06485d36e4af99294852c741705
pi <pi>
2013-02-05 01:59:48 +0000

    update setup fields and check for pre-existing monitor

--- bin/setup.py
+++ bin/setup.py
@@ -5,7 +5,7 @@
 # 
 # setup and execute airmon commands to start wireshark
 
-
+import sys
 import re
 import subprocess
 
@@ -15,7 +15,7 @@ cmd1='airmon-ng'
 arg1='check'
 arg2='wlan0'
 instr = subprocess.check_output([cmd1,arg1,arg2])
-print instr
+print  >> sys.stderr, instr
 
 # get all the pids that might interfere with airmon
 m=re.findall("\n(\d{4})\s+(\w+)",instr)
@@ -26,26 +26,38 @@ for  process in m:
         args.append(process[0])
 
 for a in args:
-    print a
-
+    print >> sys.stderr, a
 rc=0
 #kill the processes
 if len(args)>1:
-	rc = subprocess.call(args)
-	print "kill rc = " + str(rc)
+    rc = subprocess.call(args)
+    print >>sys.stderr,  "kill rc = " + str(rc)
 
-if rc == 0 :
+#is monitor already running
+cmd='ifconfig'
+res = subprocess.check_output(cmd)
+m=re.search(r"\n(mon\d)\s+Link",res)
+if m:
+	interface=m.group(1)
+else:
+	interface=None
+
+#only start monitor mode if its not already up
+if ((rc == 0) and ((interface)==None)) :
     arg1='start'
     arg2='wlan0'
     instr = subprocess.check_output([cmd1,arg1,arg2])
-    print instr    
+    print >>sys.stderr, instr    
     # get the interface that has been activated
     m=re.search(r"monitor mode enabled on\s(\w+)\)",instr)
-    interface=m.group(1)
-    print interface
-   
-    cmdstr='tshark -i mon0  -T fields -e radiotap.dbm_antsignal -e frame.len -e wlan.ta -e wlan.ra -e wlan.da -e wlan.sa -e wlan.fc.type_subtype'
-    cmd=cmdstr.split()
-    rc = subprocess.call(cmd)
-    print "tshark rc = " + str(rc)   
+    if (m is None):
+      print >> sys.stderr, "Unable to start monitor mode"
+    else:
+      interface=m.group(1)
+
+print >> sys.stderr, interface
+cmdstr="tshark -i %s -T fields -e radiotap.dbm_antsignal -e frame.len -e wlan.ta -e wlan.ra -e wlan.sa -e wlan.da -e wlan.fc.type_subtype" % (interface)
+cmd=cmdstr.split()
+rc = subprocess.call(cmd)
+print  >> sys.stderr, "tshark rc = " + str(rc)  
     
