f628068b5b7e5520ee1e90010bb32c0caf05d977
rlum <rlum>
2013-02-08 06:11:06 +0000

    rev 2 more initial file setup

--- bin/observ2.py
+++ bin/observ2.py
@@ -5,14 +5,9 @@ class Observation:
     object to accumulate signal observations into a sample
     """
     def __init__(self, sig_str,isAP=False, avgsize=30, bytes=0):
-        #self.ss_sum = sig_str
-        #self.ss_sum_squares = sig_str^2
-        #self.AP = isAP
-        #self.bytes_sum = bytes
         self.count=1
         self.avgsize=avgsize    # no of obs for rolling average
         self.rolling_samples = []
-        self.localcount=0
 
     def is_number(str):
         try:
@@ -21,26 +16,16 @@ class Observation:
         except ValueError:
             return False
 
-    def getlocalcount(self):
-        """
-        everytime value is read reset to zero. incr by add (signal sample)
-        """
-        result = self.localcount
-        self.localcount=0
-        return result
 
-
-    def add(self, sig_str, bytes=0):
+    def add(self, sig_str, bytes, subtype, time, freq, ssid, wifitype):
         if len(sig_str)>0:
             self.count+=1
-            self.localcount+=1
             #for rolling average keep last avgsize samples 
             if len(self.rolling_samples) < self.avgsize:
                 self.rolling_samples.append(int(sig_str))
             else:
                 self.rolling_samples.pop(0)
                 self.rolling_samples.append(int(sig_str))
-            self.localcount+=1
 
    # def avg_sig(self):
     #    return   self.ss_sum / self.count
@@ -51,23 +36,16 @@ class Observation:
 #
 
     def rolling_avg(self):
-        size=float(len(self.rolling_samples))
+        size=len(self.rolling_samples)
         if size>0:   
-            return float(sum(self.rolling_samples)/size)
+            return sum(self.rolling_samples)/size
         else:
             return 0
 
     def rolling_var(self):
-      if (len(self.rolling_samples) == 0):
-        return 0
-      xbar=float(self.rolling_avg())
-      var = ([ (x - xbar) for x in self.rolling_samples])
-      var = [ x*x for x in var] 
-      var = sum(var)/float(len(self.rolling_samples))
-      return var 
-      #  sumsquares = sum([ x^2 for x in self.rolling_samples])
-      #  denom =  len(self.rolling_samples) - self.rolling_avg()^2
-      #  if (denom == 0):
-      #      return 0
-      #%  else:
-      #      return sumsquares/denom
+        sumsquares = sum([ x^2 for x in self.rolling_samples])
+        denom =  len(self.rolling_samples) - self.rolling_avg()^2
+        if (denom == 0):
+            return 0
+        else:
+            return sumsquares/denom
