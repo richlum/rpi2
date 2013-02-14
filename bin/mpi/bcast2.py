d68d8523c3cde8d852e27fd491924ed21a2cd668
rlum <rlum>
2013-02-14 23:23:26 +0000

    bcast2.py works but discovered that bcast is from rank 0 to all other ranks only.  Not quite what we want.  Although the thread model will function we need to revisit what mpi call we use to send from one to rest since each node must send their own data. Also need to investigate locks to allow receiving thread to lock coallating data structure of signals from other pi nodes. This is in anticipation of a third thread which will either be timed to calculate send position data for display OR provide client service to display that calls for updates on a regular basis. Since we have a rolling average for data, this should be fine ...even if data is stale, we just need to indicate age for now...available from localpacket count which tracks the number of packets received since last refresh. Might be problematic to identify from a global time scale though since the display poll will likely be less frequend than the update polls provided by the signal data side.

--- bin/mpi/bcast2.py
+++ bin/mpi/bcast2.py
@@ -6,40 +6,42 @@ import time
 import threading
 import datetime
 
-comm = MPI.COMM_WORLD
-rank = comm.Get_rank()
 
-def bcastsnd():
-    
-    data = {'a': 7, 'b': 3.14, 'rank': rank}
-    time.sleep(1)
-    for i in range(0,3):
-        print "broadcasting from rank %d" % rank
-        comm.bcast(data, root=0)
-        time.sleep(5)
-
-def bcastrcv():    
-    buffer={} 
-    print "%d getting broadcast" % rank
-    while True:
-        buffer=comm.bcast(buffer)
-        if buffer:
-            print "r=%d: %s" % (rank, str(buffer))
-#        else:
-#           print "no data"
 
 class SndThread(threading.Thread):
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
     def run(self):
         now = datetime.datetime.now()
-        print "%s snd thread starting %s" % (self.getName(),now)
-        data = {'a': 7, 'b': 3.14, 'rank': rank}
-        bcastsnd()        
-        
+        print "%d:%s snd thread starting %s" % (rank,self.getName(),now)
+        data = {'a': 7, 'b': 3.14, 'rank': self.rank}
+        self.bcastsnd()        
+    
+    def bcastsnd(self):
+        data = {'a': 7, 'b': 3.14, 'rank': self.rank}
+        time.sleep(1)
+        for i in range(0,3):
+            print "broadcasting from rank %d" % self.rank
+            self.comm.bcast(data, root=self.rank)
+            time.sleep(5)
+
 class RcvThread(threading.Thread):
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    
     def run(self):
         now = datetime.datetime.now()
         print "%s rcvr thread starting %s" % (self.getName(), now)
-        bcastrcv()
+        self.bcastrcv()
+
+    def bcastrcv(self):    
+        buffer={} 
+        print "%d getting broadcast" % self.rank
+        while True:
+            buffer=self.comm.bcast(buffer)
+            if buffer:
+                print "r=%d: %s" % (self.rank, str(buffer))
+
 
 ########### main
 
