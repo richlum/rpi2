c90c539f0d2e22c61cdfb00878c9f619c4af62ab
yudha <yudha>
2013-03-07 21:09:12 +0000

    Updated UI, missing an element, added, works now.

--- rpi2UITest.py
+++ rpi2UITest.py
@@ -1,4 +1,4 @@
-#Updated February 27,2013
+#Updated March 7,2013
 
 import wx
 import wx.lib.plot as plot
@@ -10,17 +10,15 @@ class MyFrame(wx.Frame):
     self.panel1 = wx.Panel(self.frame1)
     self.panel1.SetBackgroundColour("green")
     
-    # taking difference between wxPython26 and wxPython28 into count
+    # Difference between wxPython26 and wxPython28 into count
     if wx.VERSION[1] < 7:
       plotter = plot.PlotCanvas(self.panel1, size=(800, 600))
     else:
       plotter = plot.PlotCanvas(self.panel1)
       plotter.SetInitialSize(size=(800, 600))
       
-    # enable the zoom feature (drag a box around area of interest)
     plotter.SetEnableZoom(True)
 
-    # list of (x,y) test file
     data = [(1,2), (2,10), (3,4), (4,5), (5,6), (6,7), (7,8), (-8,9)]
     
     marker = plot.PolyMarker(data, marker='circle')
