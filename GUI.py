00cde069aed82ba0f10631844ef68ba48ca1a6bd
yudha <yudha>
2013-03-09 22:22:08 +0000

    Updated UI element is now GUI.py. Added buffering to avoid flickering during refresh. Yudha.

---
+++ GUI.py
@@ -0,0 +1,59 @@
+import wx
+from bufferedcanvas import *
+import wx.lib.plot as plot
+
+
+
+class TestCanvas(BufferedCanvas):
+
+    def __init__(self,parent,ID=-1):
+        BufferedCanvas.__init__(self,parent,ID)
+
+
+    def draw(self, dc):
+        dc.SetBackground(wx.Brush("Black"))
+        dc.Clear()
+
+        dc.SetBrush(wx.BLUE_BRUSH)
+        dc.SetPen(wx.Pen('Red', 4))
+        dc.DrawRectangle(20,20,300,200)
+
+
+class MyFrame(wx.Frame):
+
+ def __init__(self):
+    self.frame1 = wx.Frame(None, title="Raspberry Pis' Clients Plotting", id=-1, size=(800, 600))
+    self.panel1 = wx.Panel(self.frame1)
+    self.panel1.SetBackgroundColour("green")
+    
+    MenuBar = wx.MenuBar()
+    
+    file_menu = wx.Menu()
+    
+    item = file_menu.Append(wx.ID_EXIT, text="&Exit")
+    
+    # Difference between wxPython26 and wxPython28 into count
+    if wx.VERSION[1] < 7:
+      plotter = plot.PlotCanvas(self.panel1, size=(800, 600))
+    else:
+      plotter = plot.PlotCanvas(self.panel1)
+      plotter.SetInitialSize(size=(800, 600))
+      
+    plotter.SetEnableZoom(True)
+
+    data = [(1,2), (2,10), (3,4), (4,5), (5,6), (6,7), (7,8), (-8,9)]
+    
+    marker = plot.PolyMarker(data, marker='circle')
+    
+    gc = plot.PlotGraphics([marker], 'Clients Location', 'Location by signal in X axis', 'Location by signal in Y axis')
+    plotter.Draw(gc, xAxis=(-10,10), yAxis=(-15,15))
+    
+    self.frame1.Show(True)
+
+def main():
+    app = wx.PySimpleApp()
+    frame = MyFrame()
+    app.MainLoop()
+
+if __name__ == '__main__':
+    main()
