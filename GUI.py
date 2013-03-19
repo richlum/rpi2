aed23b8262a2121834511e61eb410524995c273b
yudha <yudha>
2013-03-19 21:43:26 +0000

    updated GUI.py

--- GUI.py
+++ GUI.py
@@ -1,40 +1,24 @@
-import wx
-import wx.lib.plot as plot
+import sys
+if not hasattr(sys, 'frozen'):
+    import wxversion
+    wxversion.ensureMinimal('2.8')
 
-class rpiframe(wx.Frame):
-  
-  def __init__(self,parent,id):
-    wx.Frame.__init__(self,parent,id,'RPI-2 Group', size=(800,600))
-    self.InitUI()
-    self.Centre()
-    self.Show()
-    
-  def InitUI(self):
-  
-    self.panel=wx.Panel(self)
-    status=self.CreateStatusBar()
-    menubar=wx.MenuBar()
-    first=wx.Menu()
-    second=wx.Menu()
-    first.Append(wx.NewId(),"Quit", "Quit Program")
-    second.Append(wx.NewId(),"Refresh Data", "Refresh Data")
-    menubar.Append(first,"File")
-    menubar.Append(second,"Data")
-    self.SetMenuBar(menubar)
-    
-    plotter = plot.PlotCanvas(self.panel)
-    plotter.SetInitialSize(size=(800,600))
-    plotter.SetEnableZoom(True)
-    
-    data=[(1,2)]
-    
-    marker = plot.PolyMarker(data, marker='circle')
-    
-    gc = plot.PlotGraphics([marker], 'Clients Location', 'Location by signal in X axis', 'Location by signal in Y axis')
-    plotter.Draw(gc, xAxis=(-10,10), yAxis=(-15,15))
-        
-if __name__=='__main__':
-  app=wx.PySimpleApp()
-  frame=rpiframe(parent=None,id=-1)
-  frame.Show()
-  app.MainLoop()
+import wxmplot
+import wx
+import numpy
+
+app = wx.App()
+
+pframe = wxmplot.PlotFrame()
+z = [1,2,3,4,5,6,7,8,9]
+x = []
+y = []
+for i in range (0,len(z)):
+  x.append(z[i])
+  y.append(z[len(z)-1-i])
+  pframe.add_text(str(i),x[i],y[i],side='left',rotation=None,ha='right',va='center',size=10,color='green')
+
+pframe.scatterplot(x, y, title='Nearby Wifi Clients', size=20,xlabel='Distance (meters)',ylabel='Distance (meters)')
+pframe.Show()
+
+app.MainLoop()
